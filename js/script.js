fa7f26cd9885cffb8c3e820b4e43154563ada707
Sergey Meshchankin <smeshchankin@gmail.com>
2020-06-29 23:22:02 +0300

    [DI-001][JS] Header: populate Icons menu

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@
         { name: 'Create' }
     ];
     populator.populate('.navigation-link', menu);
+
+    const icons = [
+        { id: 'users', classes: '' },
+        { id: 'chat', classes: '' },
+        { id: 'bell', classes: '' },
+        { id: 'info', classes: 'pipe menu' }
+    ];
+    populator.populate('.icon-link', icons);
 }());
