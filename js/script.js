25f94e1f3cebf35e93121b2da4afbc6fddc48335
Sergey Meshchankin <smeshchankin@gmail.com>
2021-06-12 23:28:38 +0300

    [DI-006] Populate from json: submenu 2

--- js/script.js
+++ js/script.js
@@ -2,20 +2,11 @@
     populator.populateFromJson();
 
     const submenu = [
-        [
-            { id: 'cup', name: 'Product Design', class: '' },
-            { id: 'cup', name: 'UX &amp; UI', class: '' },
-            { id: 'cup', name: 'Figma', class: '' },
-            { id: 'cup', name: 'Ant Design', class: '' }
-        ],
-        [
-            { id: 'pages', name: 'Pages', class: '' },
-            { id: 'groups', name: 'Groups', class: '' },
-            { id: 'events', name: 'Events', class: '' },
-            { id: 'games', name: 'Games', class: '' }
-        ]
+        { id: 'pages', name: 'Pages', class: '' },
+        { id: 'groups', name: 'Groups', class: '' },
+        { id: 'events', name: 'Events', class: '' },
+        { id: 'games', name: 'Games', class: '' }
     ];
 
-    populator.populate('.submenu-2', submenu[1]);
-    populator.populate('.submenu-3', submenu[2]);
+    populator.populate('.submenu-3', submenu);
 }());
