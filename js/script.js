e27e049d0cfd7e1937c09c3edbcc8456b361db2f
Sergey Meshchankin <smeshchankin@gmail.com>
2021-06-13 23:02:58 +0300

    [DI-006] Populate from json: submenu 3

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
 (function() {
     populator.populateFromJson();
-
-    const submenu = [
-        { id: 'pages', name: 'Pages', class: '' },
-        { id: 'groups', name: 'Groups', class: '' },
-        { id: 'events', name: 'Events', class: '' },
-        { id: 'games', name: 'Games', class: '' }
-    ];
-
-    populator.populate('.submenu-3', submenu);
 }());
